9b1559543679a7aefb77269bf81ef53c691901ec
Aritra-Paul <aritrapaul48.ap@gmail.com>
2020-02-21 13:09:02 -0500

    Added the EditHosts Class

--- forms/hosts_forms/edit_existing_hosts.py
+++ forms/hosts_forms/edit_existing_hosts.py
@@ -1,9 +1,43 @@
 import npyscreen
 from overrides.constants import *
-from .addgroup import AddGroup
+from .addhosts import AddHosts
+from .selecthosts import SelectHosts
 
-class EditHosts(AddGroup):
+class EditHosts(AddHosts, SelectHosts):
     def create(self):
-        super().create()
-        groupName = self.add(npyscreen.TitleText, name="Group Name:", value="", editable=True,
-                 begin_entry_at=70, )
+        SelectHosts.create(self)
+        self.confirmButton = self.add(npyscreen.ButtonPress, name="Confirm Host to edit", value= "", )
+        self.confirmButton.whenPressed = self.show_edit_delete_screen
+        AddHosts.create(self)
+        self.deleteHosts = self.add(npyscreen.ButtonPress, name="DELETE HOSTS", )
+        self.deleteHosts.whenPressed = self.on_ok
+
+        self.deleteHosts.hidden = True
+        self.select_hosts_title = True
+        self.add_hosts_title.hidden = True
+        self.add_hosts_title2.hidden= True
+        self.add_hosts_title3.hidden= True
+        self.add_hosts_title4.hidden= True
+        self.add_hosts_password.hidden= True
+        self.add_hosts_json.hidden=True
+
+    def show_edit_delete_screen(self):
+        self.confirmButton.hidden = True
+        self.select_hosts_options = True
+
+        self.deleteHosts.hidden = False
+        self.select_hosts_title = False
+        self.add_hosts_title.hidden = False
+        self.add_hosts_title2.hidden= False
+        self.add_hosts_title3.hidden= False
+        self.add_hosts_title4.hidden= False
+        self.add_hosts_password.hidden= False
+        self.add_hosts_json.hidden= False
+        self.DISPLAY()
+
+
+
+
+
+        # groupName = self.add(npyscreen.TitleText, name="Group Name:", value="", editable=True,
+        #          begin_entry_at=70, )
--- forms/hosts_forms/selecthosts.py
+++ forms/hosts_forms/selecthosts.py
@@ -3,8 +3,8 @@ import npyscreen
 
 class SelectHosts(npyscreen.ActionForm):
     def create(self):
-        self.add(npyscreen.TitleText, name="Please select host(s):", value="", editable=False, begin_entry_at=70, )
-        self.add(npyscreen.MultiSelect, max_height=4, value=[1, 2],
+        self.select_hosts_title = self.add(npyscreen.TitleText, name="Please select host(s):", value="", editable=False, begin_entry_at=70, )
+        self.select_hosts_options = self.add(npyscreen.MultiSelect, max_height=4, value=[1, 2],
                  values=["Host1", "Host2", "Host3"], scroll_exit=True, width=20)
 
     def on_ok(self):
