a12044012a85380b0c035cbbdfaef62bf86e385f
AZhylkaidarov <AZhylkaidarov>
2020-02-25 08:37:04 -0500

    Add top directory to path for importing deployer.py

--- tests/test_launch_application.py
+++ tests/test_launch_application.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 import curses
 import npyscreen
+import sys
+import os.path
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 import deployer
 import pytest
 
