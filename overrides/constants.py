659e11394745538af1bcd91afb63f46c28fc23ce
l1ghtman2k <aibek.zhil@gmail.com>
2020-02-26 23:55:52 -0500

    Constants for profiles, and methods

--- overrides/constants.py
+++ overrides/constants.py
@@ -1,3 +1,7 @@
+import os
+import json
+from definitions import ROOT_DIR
+
 ADD_HOSTS = "AddHosts"
 ADD_GROUP = "AddGroup"
 SELECTED_HOSTS = "SelectHosts"
@@ -10,16 +14,34 @@ MAIN_MENU_TITLE = "Menu"
 SAVE_BUTTON_TEXT = "Save"
 EDIT_HOSTS = "EditHosts"
 EDIT_GROUP = "EditGroup"
-PROFILE = 'Profiles'
-modifications_made = False
 
-import os
-from definitions import ROOT_DIR
+SAVE_PROFILE = "SaveProfile"
+SELECT_PROFILE = "SelectProfile"
+
+
+METHOD = None
+HOSTS = None
+
+HOSTS_CONFIG = None
+GROUPS_CONFIG = None
+PROFILE_CONFIG = None
+CHANGES_PENDING = False
+
 list_of_method_subfolders = [file for file in os.scandir(os.path.join(ROOT_DIR, 'modules')) if file.is_dir()]
 list_of_method_names = []
-method_module_map = {}
+method_to_module_map = {}
 
 for file in list_of_method_subfolders:
     list_of_method_names.append(file.name)
-    method_module_map[file.name] = []
+    method_to_module_map[file.name] = []
+    for module in [file for file in os.scandir(os.path.join(ROOT_DIR, 'modules', file.name)) if file.is_dir()]:
+        method_to_module_map[file.name].append(module.name)
 
+list_of_profiles_subfolders = [file for file in os.scandir(os.path.join(ROOT_DIR, 'profiles')) if file.is_dir()]
+list_of_profile_names = []
+profiles_properties = {}
+
+for file in list_of_profiles_subfolders:
+    list_of_profile_names.append(file.name)
+    with open(os.path.join(file, 'config.json')) as f:
+        profiles_properties[file.name] = json.loads(f.read())
