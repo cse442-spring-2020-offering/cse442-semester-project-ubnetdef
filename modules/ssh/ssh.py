d5fad49058012abdb1cfd0aab35f2e8d9d556295
AZhylkaidarov <AZhylkaidarov>
2020-03-04 16:25:28 -0500

    boilerplate methods for connection SSH using parallel SSH library

--- modules/ssh/ssh.py
+++ modules/ssh/ssh.py
@@ -1,2 +1,16 @@
+from pssh.clients import ParallelSSHClient
+import overrides.shared_variables as HOSTS_CONFIG
+
 class ConnectionSSH():
-    pass
+    def __init__(self, hosts):
+        self.hosts = hosts
+
+    def run(self):
+        pass
+
+    def version_detection(self):
+        pass
+        output = self.client.run_command('uname')
+        for host, host_output in output.items():
+            for line in host_output.stdout:
+                print(line)
